a58b54f7b7fc9f2b9698066f01c5eae7875c2c38
maxsun03 <max.sunkangshu@gmail.com>
2018-02-09 16:54:33 -0400

    Stage2

--- app/src/main/java/com/example/kangshusun/validator/Validator.java
+++ app/src/main/java/com/example/kangshusun/validator/Validator.java
@@ -2,17 +2,32 @@ package com.example.kangshusun.validator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.*;
 
 public class Validator extends AppCompatActivity {
 
     public static int Validator(String p){
-        int success=0;
-        if(p.length()>=8) {
+        char sc[]=new char[12];
+        for(int i=0; i<p.length(); i++)
+            sc[i]=p.charAt(i);
+            int success=0;
+        if(p.length()>=8)
             success++;
-            if (p.equalsIgnoreCase("password")) {
+        if(p.equalsIgnoreCase("password"))
+            success++;
+        if(p.length()<13)
+            success++;
+        for(int i=0; i<13; i++) {
+            if (Character.isDigit(sc[i])) {
                 success++;
+                break;
             }
-            return success;
+        }
+        for(int j=0; j<13; j++) {
+            if (Character.isUpperCase(sc[j])) {
+                success++;
+                break;
+            }
         }
         return success;
     }
